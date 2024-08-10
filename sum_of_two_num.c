4d0848ba8fb0ae94abf558aeaf99eb26374a2101
Rohitdandge <rohitpatil143d@gmail.com>
2024-08-10 21:18:56 +0530

    Add files via upload

---
+++ sum_of_two_num.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    int b;
+    printf(" enter the first num:");
+    scanf("%d",&a);
+    printf("enter the second num:");
+    scanf("%d",&b);
+    int z= a+b;
+    printf("the sum of two num is: %d",z);
+     return 0;
+ }
