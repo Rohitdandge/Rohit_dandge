5747e65139a4e5b35e39a8d98338b806b19aee7e
Rohitdandge <rohitpatil143d@gmail.com>
2024-08-10 22:36:50 +0530

    Add files via upload

---
+++ shortcut_float.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    float a,b,c;
+    printf("enter the first num in float decimal:");
+    scanf("%f",&a);
+    printf("enter the second num in float decimal:");
+    scanf("%f",&b);
+    c = a+b;
+    printf("the sum of two decimal num is: %f",c);
+     return 0;
+ }
