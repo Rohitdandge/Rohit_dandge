ef0ade9f2915c2ea131eccb589ed1ed620664b2d
Rohitdandge <rohitpatil143d@gmail.com>
2024-08-10 21:49:56 +0530

    Add files via upload

---
+++ addition_num.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,sum;
+    printf("enter the two num:");
+    scanf("%d %d",&num1,&num2);
+    sum = num1+num2;
+    printf("the sum of two num:%d\n",sum);
+     return 0;
+ }
