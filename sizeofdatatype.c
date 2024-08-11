05b66c16ca77381b6b1b2b9e8aa366c5d29c2245
Rohitdandge <rohitpatil143d@gmail.com>
2024-08-11 21:13:42 +0530

    Add files via upload

---
+++ sizeofdatatype.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    printf("the size of int %zu bytes \n",sizeof(int));
+    printf("the size of float %zu bytes\n",sizeof(float));
+    printf("the size of char %zu bytes \n",sizeof(char));
+    printf("the size of double %zu bytes \n",sizeof(double));
+
+    return 0;
+
+
+
+
+
+
+}
